040924c86abb1bdae5ccb2f8731b5d37adb988b2
Noah981 <101044788+Noah981@users.noreply.github.com>
2026-09-18 16:48:02 +0900

    Align Android Java and Kotlin JVM targets to 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,13 @@ android {
         versionName = "0.1.0"
     }
     buildFeatures { compose = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
